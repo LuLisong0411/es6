5d8b7cd980c307e545a26391a2e991a70ddfee7c
陆力松 <lulisong0411@163.com>
2019-05-08 21:38:38 +0800

    解构赋值

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 //var是全局变量，会污染全局
 // var a = 1;
 // console.log(a);
@@ -42,9 +44,43 @@
 //解构的默认值：解构赋值是允许你使用默认值的
 // let [foo = true] =[];
 // console.log(foo); //控制台打印出true
-var _ref = ['100'],
-    a = _ref[0],
-    _ref$ = _ref[1],
-    b = _ref$ === undefined ? "LuLisong" : _ref$;
+// let [a,b="LuLisong"]=['100']
+// console.log(a+b); //控制台显示“100LuLisong”
+//undefined和null的区别：undefined相当于什么都没有，b是默认值;null相当于有值，但值为null。所以b并没有取默认值，而是解构成了null。
+// let [a,b="LuLisong"]=['黑龙江大学',undefined];
+// console.log(a+b); //控制台显示“黑龙江大学LuLisong”
+// let [a,b="LuLisong"]=['黑龙江大学',null];
+// console.log(a+b); //控制台显示“黑龙江大学null”
+//对象的解构赋值
+//对象的解构与数组有一个重要的不同。数组的元素是按次序排列的，变量的取值由它的位置决定；而对象的属性没有次序，变量必须与属性同名，才能取到正确的值。
+// let {foo,bar} = {foo:'LuLisong',bar:'黑龙江大学'};
+// console.log(foo+bar); //控制台打印出了“LuLisong黑龙江大学”
+// 如果在解构之前就定义了变量，这时候你再解构会出现问题(圆括号的使用可以解决)
+//错误示范
+// let foo;
+// {foo} ={foo:'LuLisong'};
+// console.log(foo);
+//正确示范
+// let foo;
+// ({foo} ={foo:'LuLisong'});
+// console.log(foo); //控制台输出LuLisong
+//字符串解构：字符串也可以解构，这是因为，此时字符串被转换成了一个类似数组的对象。
+var _LuLisong = "LuLisong",
+    _LuLisong2 = _slicedToArray(_LuLisong, 8),
+    a = _LuLisong2[0],
+    b = _LuLisong2[1],
+    c = _LuLisong2[2],
+    d = _LuLisong2[3],
+    e = _LuLisong2[4],
+    f = _LuLisong2[5],
+    g = _LuLisong2[6],
+    h = _LuLisong2[7];
 
-console.log(a + b); //控制台显示“LuLisong100”
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
--- src/index.js
+++ src/index.js
@@ -42,3 +42,31 @@
 // console.log(foo); //控制台打印出true
 // let [a,b="LuLisong"]=['100']
 // console.log(a+b); //控制台显示“100LuLisong”
+//undefined和null的区别：undefined相当于什么都没有，b是默认值;null相当于有值，但值为null。所以b并没有取默认值，而是解构成了null。
+// let [a,b="LuLisong"]=['黑龙江大学',undefined];
+// console.log(a+b); //控制台显示“黑龙江大学LuLisong”
+// let [a,b="LuLisong"]=['黑龙江大学',null];
+// console.log(a+b); //控制台显示“黑龙江大学null”
+//对象的解构赋值
+//对象的解构与数组有一个重要的不同。数组的元素是按次序排列的，变量的取值由它的位置决定；而对象的属性没有次序，变量必须与属性同名，才能取到正确的值。
+// let {foo,bar} = {foo:'LuLisong',bar:'黑龙江大学'};
+// console.log(foo+bar); //控制台打印出了“LuLisong黑龙江大学”
+// 如果在解构之前就定义了变量，这时候你再解构会出现问题(圆括号的使用可以解决)
+//错误示范
+// let foo;
+// {foo} ={foo:'LuLisong'};
+// console.log(foo);
+//正确示范
+// let foo;
+// ({foo} ={foo:'LuLisong'});
+// console.log(foo); //控制台输出LuLisong
+//字符串解构：字符串也可以解构，这是因为，此时字符串被转换成了一个类似数组的对象。在实战项目中解构Json数据格式还是很普遍的，有了ES6得帮助，提高了不少工作效率
+const [a,b,c,d,e,f,g,h]="LuLisong";
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
