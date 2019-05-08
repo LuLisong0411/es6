f81e5edc50faecb249e1f308af45c78de4fdfb2b
陆力松 <lulisong0411@163.com>
2019-05-08 21:26:29 +0800

    解构赋值

--- dist/index.js
+++ dist/index.js
@@ -20,7 +20,31 @@
 //     console.log("循环1内："+i)
 // }
 // console.log("循环1外："+i)
-for (var _i = 0; _i < 10; _i++) {
-    console.log("循环2内：" + _i);
-}
-console.log("循环2外：" + i);
+// for(let i=0;i<10;i++){
+//     console.log("循环2内："+i)
+// }
+// console.log("循环2外："+i)
+//const是声明的常量，不能被let或var在改变值
+// const a = "lulisong";
+// var a = 2;
+// console.log(a)
+//解构赋值
+// let [a,b,c] = [1,2,3]
+// console.log(a);
+// console.log(b);
+// console.log(c)
+//数组模式和赋值模式统一：等号左边和等号右边的形式要统一，如果不统一解构将失败
+// let [a,[b,c],d]=[1,[2,3],4];
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+//解构的默认值：解构赋值是允许你使用默认值的
+// let [foo = true] =[];
+// console.log(foo); //控制台打印出true
+var _ref = ['100'],
+    a = _ref[0],
+    _ref$ = _ref[1],
+    b = _ref$ === undefined ? "LuLisong" : _ref$;
+
+console.log(a + b); //控制台显示“LuLisong100”
--- src/index.js
+++ src/index.js
@@ -26,3 +26,19 @@
 // const a = "lulisong";
 // var a = 2;
 // console.log(a)
+//解构赋值
+// let [a,b,c] = [1,2,3]
+// console.log(a);
+// console.log(b);
+// console.log(c)
+//数组模式和赋值模式统一：等号左边和等号右边的形式要统一，如果不统一解构将失败
+// let [a,[b,c],d]=[1,[2,3],4];
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+//解构的默认值：解构赋值是允许你使用默认值的
+// let [foo = true] =[];
+// console.log(foo); //控制台打印出true
+// let [a,b="LuLisong"]=['100']
+// console.log(a+b); //控制台显示“100LuLisong”
